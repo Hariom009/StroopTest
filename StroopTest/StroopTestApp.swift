2aa391d650974374ee98a25f2cde02850dfb8481
Hariom009 <hariomconnects03@gmail.com>
2025-05-27 15:31:58 +0530

    Initial Commit

---
+++ StroopTest/StroopTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StroopTestApp.swift
+//  StroopTest
+//
+//  Created by Hari's Mac on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct StroopTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StroopTestTests/StroopTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  StroopTestTests.swift
+//  StroopTestTests
+//
+//  Created by Hari's Mac on 27.05.2025.
+//
+
+import Testing
+@testable import StroopTest
+
+struct StroopTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
